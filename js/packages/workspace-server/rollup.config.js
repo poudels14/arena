5ee3b1e739e53e81d569fb494411dc915e36cf7b
Sagar <poudels14@gmail.com>
2023-02-21 23:28:57 -0800

    Use rollup-plugin-typescript2
      - @rollup/plugin-typescript was acting up

--- js/packages/workspace-server/rollup.config.js
+++ js/packages/workspace-server/rollup.config.js
@@ -2,7 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 // import commonjs from "@rollup/plugin-commonjs";
 // import json from "@rollup/plugin-json";
 import terser from "@rollup/plugin-terser";
-import typescript from "@rollup/plugin-typescript";
+import typescript from "rollup-plugin-typescript2";
 
 export default {
   input: {
