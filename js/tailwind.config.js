3fb330455b03d3b7adbe7706fdd8053a663a0a5d
Sagar <poudels14@gmail.com>
2023-04-13 16:04:26 -0700

    install tailwind for autocomplete
      - even though twind is used for styling, this tailwind setup
        enables auto-complete in VSCode

---
+++ js/tailwind.config.js
@@ -0,0 +1,6 @@
+// Note(sagar): adding this for VScode auto-complete
+// even though tailwind isn't used
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,css,js,jsx,ts,tsx}"],
+};
