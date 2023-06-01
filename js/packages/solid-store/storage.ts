2188fb4152db7f0ee7ee2780010c02f2b2a69917
Sagar <poudels14@gmail.com>
2023-05-31 20:55:12 -0700

    solid-store: add synced store file
      - forgot to add this file earlier

---
+++ js/packages/solid-store/storage.ts
@@ -0,0 +1,19 @@
+import { createEffect } from "solid-js";
+import { createStore } from "./index";
+
+/**
+ * Creates a store that's synced with local storage
+ */
+function createSyncedStore<T>(initValue: T, options: { storeKey: string }) {
+  const cache = localStorage.getItem(options.storeKey);
+  if (cache) {
+    initValue = JSON.parse(cache);
+  }
+  const [store, setStore] = createStore<T>(initValue);
+  createEffect(() => {
+    localStorage.setItem(options.storeKey, JSON.stringify(store()));
+  });
+  return [store, setStore] as ReturnType<typeof createStore<T>>;
+}
+
+export { createSyncedStore };
