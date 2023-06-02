de164c7cb8f600c99160e3edb9923e362fb9341a
Sagar <poudels14@gmail.com>
2023-06-01 20:29:21 -0700

    db: add drizzle integration for @arena/db/pg
      - can do db = drizzle(client) and use `db` to query

---
+++ js/packages/db/pg/index.ts
@@ -0,0 +1,39 @@
+import {
+  AnyColumn,
+  SelectedFields,
+  Table,
+  sql as drizzleSql,
+} from "drizzle-orm";
+import { PgDialect } from "drizzle-orm/pg-core";
+import { QueryBuilder } from "drizzle-orm/pg-core";
+
+type SQLResult = { sql: string; params: any[] };
+type SQL = (tag: TemplateStringsArray, ...args: any[]) => SQLResult;
+
+const pg = new PgDialect();
+
+const sql: SQL & typeof drizzleSql = Object.assign(
+  (tag: TemplateStringsArray, ...args: any[]): SQLResult => {
+    const sqlQuery = drizzleSql(tag, ...args);
+    return pg.sqlToQuery(sqlQuery);
+  },
+  drizzleSql
+);
+
+function select<T>(): ReturnType<QueryBuilder["select"]>;
+function select<C extends AnyColumn, T extends Table>(
+  fields?: SelectedFields<C, T>[]
+) {
+  const qb = new QueryBuilder();
+  if (fields) {
+    // @ts-expect-error
+    return qb.select(fields);
+  }
+  return qb.select();
+}
+
+export { sql, select };
+export { drizzle } from "./drizzle";
+export * from "drizzle-orm/pg-core";
+export { eq } from "drizzle-orm";
+export type { InferModel } from "drizzle-orm";
