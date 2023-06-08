a5122e5c5846467397f8023bed609a7ad6482e13
Sagar <poudels14@gmail.com>
2023-06-08 16:15:45 -0700

    uikit: use random id of length 13 instead of 22
      - 22 is too long and is unnecessary

--- js/packages/uikit/uniqueId.ts
+++ js/packages/uikit/uniqueId.ts
@@ -1,6 +1,6 @@
 import { customAlphabet } from "nanoid";
 
-const alphabet = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghjkmnpqrstuvwxyz";
-const uniqueId = customAlphabet(alphabet, 22);
+const alphabet = "123456789ABCDEFGHJKMNPQRSTUVWXYZabcdefghjkmnpqrstuvwxyz";
+const uniqueId = customAlphabet(alphabet, 13);
 
 export { uniqueId };
