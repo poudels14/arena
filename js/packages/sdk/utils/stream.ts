8c238aef6df0fb76e4edd188b72eea4fe9a9f1e8
Sagar <poudels14@gmail.com>
2023-08-09 02:27:46 -0700

    sdk/utils: helper to convert stream to async iter

---
+++ js/packages/sdk/utils/stream.ts
@@ -0,0 +1,28 @@
+/**
+ * This converts a readable stream with JSON data to async iterable.
+ * Returns `{ json: data }` with parsed data as the json field.
+ *
+ * If parsing the data as JSON fails, it will return `{ text }`
+ */
+async function* jsonStreamToAsyncIterator(stream: ReadableStream) {
+  const reader = stream.getReader();
+  let data = await reader?.read();
+  var enc = new TextDecoder("utf-8");
+  while (data && !data.done) {
+    const text = enc.decode(data?.value);
+    const lines = text.split(/\r?\n/);
+    for (let line of lines) {
+      if (line.startsWith("data:")) {
+        line = line.substring(5);
+        try {
+          yield { json: JSON.parse(line) };
+        } catch (e) {
+          yield { text: line.trim() };
+        }
+      }
+    }
+    data = await reader?.read();
+  }
+}
+
+export { jsonStreamToAsyncIterator };
