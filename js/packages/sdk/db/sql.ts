edea6140fc616ef4c73d0c3ecf809b7797759a24
Sagar <poudels14@gmail.com>
2024-01-03 23:55:11 -0800

    moved sdk to @portal/sdk

--- js/packages/sdk/db/sql.ts
+++
@@ -1,7 +0,0 @@
-type QueryClient = {
-  query<T>(sql: string, parameters?: any[]): Promise<{ rows: T[] }>;
-};
-
-export type SqlDatabaseClient = {
-  transaction<T>(closure: () => T | Promise<T>): Promise<void>;
-} & QueryClient;
--- js/packages/sdk/db/sqlite/index.ts
+++
@@ -1,1 +0,0 @@
-export type { SqliteDatabaseClient, SqliteDatabaseConfig } from "./types";
