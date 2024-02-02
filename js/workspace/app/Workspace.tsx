deaea8087afe008a57dc33efbb0144fe0dc07cb6
Sagar <poudels14@gmail.com>
2024-02-02 00:56:20 -0800

    workspace: show ChatIsland when chat context is set

--- js/workspace/app/Workspace.tsx
+++ js/workspace/app/Workspace.tsx
@@ -1,43 +1,101 @@
-import { Show, lazy, createSelector, createMemo } from "solid-js";
+import {
+  Show,
+  lazy,
+  createSelector,
+  createMemo,
+  createSignal,
+  createEffect,
+  createComputed,
+  Setter,
+} from "solid-js";
 import { SidebarTab } from "@portal/solid-ui/sidebar";
 import { Sidebar as PortalSidebar } from "@portal/solid-ui/sidebar";
-import { Router, Route, useNavigate, useMatcher } from "@portal/solid-router";
+import {
+  Router,
+  Route,
+  useNavigate,
+  useMatcher,
+  useLocation,
+} from "@portal/solid-router";
 import { HiOutlineDocumentChartBar, HiOutlineHome } from "solid-icons/hi";
 import { QueryContextProvider } from "@portal/solid-query";
-import { WorkspaceContextProvider, useWorkspaceContext } from "./context.tsx";
+import { SharedWorkspaceContextProvider } from "@portal/workspace-sdk";
+import {
+  ChatContextProvider,
+  ChatIsland,
+} from "@portal-apps/assistant/app/chat";
+import { WorkspaceContextProvider, useWorkspaceContext } from "./context";
 
 const Home = lazy(() => import("./home/index.tsx"));
-const Documents = lazy(() => import("./documents/index.tsx"));
 const AtlasAI = lazy(() => import("@portal-apps/assistant/app"));
+const PortalDrive = lazy(() => import("@portal-apps/drive/app"));
 
 const Workspace = () => {
+  const [getActiveApp, setActiveApp] = createSignal(null);
   return (
-    <div class="h-screen flex flex-row">
-      <Router>
-        <QueryContextProvider urlPrefix="/">
-          <WorkspaceContextProvider>
-            <WorkspaceSidebar />
-            <WorkspaceRouter />
-          </WorkspaceContextProvider>
-        </QueryContextProvider>
-      </Router>
-    </div>
+    <SharedWorkspaceContextProvider activeApp={getActiveApp()}>
+      <div class="h-screen flex flex-row">
+        <Router>
+          <QueryContextProvider urlPrefix="/">
+            <WorkspaceContextProvider>
+              <WorkspaceSidebar />
+              <WorkspaceRouter setActiveApp={setActiveApp} />
+            </WorkspaceContextProvider>
+          </QueryContextProvider>
+        </Router>
+      </div>
+    </SharedWorkspaceContextProvider>
   );
 };
 
-const WorkspaceRouter = () => {
+const WorkspaceRouter = (props: { setActiveApp: Setter<any> }) => {
   const { activeWorkspace } = useWorkspaceContext();
+
+  const portalDrive = createMemo(() => {
+    const apps = activeWorkspace.apps();
+    return apps.find((app) => app.slug == "portal_drive");
+  });
+
   const atlasAi = createMemo(() => {
     const apps = activeWorkspace.apps();
     return apps.find((app) => app.slug == "atlas_ai");
   });
+
+  const [getActiveThreadId, setActiveThreadId] = createSignal<
+    string | undefined
+  >(undefined);
+  const [hideChatIsland, setHideChatIsland] = createSignal<boolean>(false, {
+    equals() {
+      // we want to retrigger hide every time location changes
+      return false;
+    },
+  });
+  const location = useLocation();
+  createEffect(() => {
+    // if url is updated, reset active thread id for ChatIsland
+    void location.pathname;
+    setHideChatIsland(true);
+    setActiveThreadId(undefined);
+  });
   return (
-    <main class="content flex-1">
-      <Route path="/documents">
-        <Documents />
+    <main class="relative content flex-1">
+      <Route path="/drive">
+        <Show when={portalDrive()}>
+          <CurrentAppSetter
+            setActiveApp={props.setActiveApp}
+            app={portalDrive()!}
+          />
+          <QueryContextProvider urlPrefix={`/w/apps/${portalDrive()!.id}/`}>
+            <PortalDrive />
+          </QueryContextProvider>
+        </Show>
       </Route>
       <Route path="/chat">
         <Show when={atlasAi()}>
+          <CurrentAppSetter
+            setActiveApp={props.setActiveApp}
+            app={atlasAi()!}
+          />
           <QueryContextProvider urlPrefix={`/w/apps/${atlasAi()!.id}/api/`}>
             <AtlasAI />
           </QueryContextProvider>
@@ -46,8 +104,26 @@ const WorkspaceRouter = () => {
       <Route path="/" exact>
         <Home />
       </Route>
+      <QueryContextProvider urlPrefix={`/w/apps/${atlasAi()!.id}/api/`}>
+        <ChatContextProvider
+          activeThreadId={getActiveThreadId()}
+          singleThreadOnly={true}
+          onThreadReady={(threadId) => {
+            setActiveThreadId(threadId);
+          }}
+        >
+          <ChatIsland hide={hideChatIsland()} />
+        </ChatContextProvider>
+      </QueryContextProvider>
     </main>
   );
+};
+
+const CurrentAppSetter = (props: { setActiveApp: Setter<any>; app: any }) => {
+  createComputed(() => {
+    props.setActiveApp(props.app);
+  });
+  return <></>;
 };
 
 const WorkspaceSidebar = () => {
@@ -74,12 +150,12 @@ const WorkspaceSidebar = () => {
         icon={{
           svg: <HiOutlineDocumentChartBar />,
         }}
-        active={isTab("documents")}
+        active={isTab("drive")}
         onClick={() => {
-          navigate("/documents");
+          navigate("/drive");
         }}
       >
-        <div>Documents</div>
+        <div>Drive</div>
       </SidebarTab>
       <SidebarTab
         icon={{
