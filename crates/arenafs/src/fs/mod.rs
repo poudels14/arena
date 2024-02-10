8c3150039f14bf228a5279a5a9808861aebab1ac
Sagar <poudels14@gmail.com>
2024-02-09 16:29:32 -0800

    arenafs: mkdir + rmdir + etc working

--- crates/arenafs/src/fs/mod.rs
+++ crates/arenafs/src/fs/mod.rs
@@ -1,25 +1,43 @@
 use std::ffi::OsStr;
 use std::sync::Arc;
-use std::time::{Duration, Instant, UNIX_EPOCH};
+use std::time::{Duration, Instant, SystemTime};
 
-use chrono::Utc;
+use chrono::{DateTime, Utc};
 use fuser::{
   FileType, ReplyAttr, ReplyData, ReplyDirectory, ReplyEmpty, ReplyEntry,
   ReplyWrite, ReplyXattr, Request,
 };
+use once_cell::sync::Lazy;
 
 use crate::backend::{Backend, DbAttribute};
 use crate::error::Error;
+
+static NANOID_CHARS: Lazy<Vec<char>> = Lazy::new(|| {
+  "123456789ABCDEFGHJKMNPQRSTUVWXYZabcdefghjkmnpqrstuvwxyz"
+    .chars()
+    .collect()
+});
 
 #[derive(Debug, Default)]
 struct Node {
   attr: DbAttribute,
   cached_at: Option<Instant>,
+  archived_at: Option<Instant>,
 }
 
+#[derive(Debug)]
 struct CachedFile {
   id: String,
   is_new: bool,
+  content: Vec<u8>,
+}
+
+#[allow(unused)]
+#[derive(Debug)]
+pub struct NewFile {
+  id: String,
+  parent_id: Option<String>,
+  path: String,
   content: Vec<u8>,
 }
 
@@ -46,7 +64,7 @@ impl FileSystem {
     nodes.push(Node {
       attr: DbAttribute {
         // Set this to absurd value so that it's not matched during lookup
-        parent_id: Some(nanoid::nanoid!(21)),
+        parent_id: Some(nanoid::nanoid!(21, &NANOID_CHARS)),
         ..Default::default()
       },
       ..Default::default()
@@ -66,8 +84,7 @@ impl FileSystem {
       .await?
       .unwrap_or_else(|| DbAttribute {
         id: "/".to_owned(),
-        // Set this to absurd value so that it's not matched during lookup
-        parent_id: Some(nanoid::nanoid!(21)),
+        parent_id: Some("/dev/null".to_owned()),
         is_directory: true,
         ..Default::default()
       });
@@ -107,6 +124,60 @@ impl FileSystem {
     Ok(())
   }
 
+  pub fn list_new_files(&self) -> Vec<NewFile> {
+    self
+      .cached_files
+      .iter()
+      .filter(|file| file.is_new)
+      .filter_map(|file| {
+        let node = self
+          .nodes
+          .iter()
+          .find(|node| node.attr.id == file.id && node.archived_at.is_none());
+        node.filter(|n| !n.attr.is_directory).map(|node| {
+          // if parent id is root, set it to none
+          let parent_id = node
+            .attr
+            .parent_id
+            .as_ref()
+            .filter(|id| id.as_str() != "/")
+            .cloned();
+          NewFile {
+            id: file.id.clone(),
+            parent_id,
+            path: self
+              .get_file_path(self.find_node_index(Some(&file.id)).unwrap()),
+            content: file.content.clone(),
+          }
+        })
+      })
+      .collect()
+  }
+
+  fn get_file_path(&self, ino: usize) -> String {
+    let mut components = vec![];
+    let mut node = &self.nodes[ino];
+    components.push(node.attr.name.clone());
+    loop {
+      let parent_ino = self.find_node_index(node.attr.parent_id.as_ref());
+      match parent_ino {
+        Some(ino) => {
+          let parent = &self.nodes[ino];
+          if parent.attr.id.as_str() == "/" {
+            // do this to not include the dummy root id
+            break;
+          }
+          components.push(parent.attr.name.to_owned());
+          node = parent;
+        }
+        None => break,
+      }
+    }
+    components.push("".to_owned());
+    components.reverse();
+    components.join("/")
+  }
+
   #[inline]
   fn find_node_index(&self, id: Option<&String>) -> Option<usize> {
     self.nodes.iter().position(|node| Some(&node.attr.id) == id)
@@ -118,10 +189,13 @@ impl FileSystem {
       ino: ino as u64,
       size: attr.size as u64,
       blocks: 1,
-      atime: UNIX_EPOCH,
-      mtime: UNIX_EPOCH,
-      ctime: UNIX_EPOCH,
-      crtime: UNIX_EPOCH,
+      atime: SystemTime::now(),
+      mtime: DateTime::<Utc>::from_naive_utc_and_offset(attr.updated_at, Utc)
+        .into(),
+      ctime: DateTime::<Utc>::from_naive_utc_and_offset(attr.updated_at, Utc)
+        .into(),
+      crtime: DateTime::<Utc>::from_naive_utc_and_offset(attr.created_at, Utc)
+        .into(),
       kind: if attr.is_directory {
         FileType::Directory
       } else {
@@ -135,6 +209,15 @@ impl FileSystem {
       flags: 0,
       blksize: 512,
     }
+  }
+
+  fn find_child_ino(&self, parent_id: &str, child: &OsStr) -> Option<usize> {
+    self.nodes.iter().position(|node| {
+      let node = &node.attr;
+      child.eq(node.name.as_str())
+        && parent_id
+          == node.parent_id.as_ref().map(|s| s.as_str()).unwrap_or("/")
+    })
   }
 
   fn update_file_content_cache(
@@ -162,6 +245,33 @@ impl FileSystem {
         })
       }
     }
+  }
+
+  /// Returns ino of the new node
+  fn add_new_node(
+    &mut self,
+    parent_id: &str,
+    name: &OsStr,
+    is_directory: bool,
+  ) -> usize {
+    let attr = DbAttribute {
+      id: nanoid::nanoid!(21, &NANOID_CHARS),
+      name: name.to_str().expect("Unsupported file name").to_owned(),
+      parent_id: Some(parent_id.to_owned()),
+      created_at: Utc::now().naive_utc(),
+      is_directory,
+      ..Default::default()
+    };
+    self.cached_files.push(CachedFile {
+      id: attr.id.to_owned(),
+      content: vec![],
+      is_new: true,
+    });
+    self.nodes.push(Node {
+      attr,
+      ..Default::default()
+    });
+    self.nodes.len() - 1
   }
 }
 
@@ -198,22 +308,19 @@ impl fuser::Filesystem for FileSystem {
       self.load_children_nodes(parent).unwrap();
     }
 
-    let file_index = self.nodes.iter().position(|node| {
-      let node = &node.attr;
-      name.eq(node.name.as_str())
-        && parent_node_id.as_str()
-          == node.parent_id.as_ref().map(|s| s.as_str()).unwrap_or("/")
-    });
-
+    let file_index = self.find_child_ino(&parent_node_id, name);
     match file_index {
       Some(ino) => {
         let node = &self.nodes[ino];
-        reply.entry(&TTL, &self.get_fuser_attr(ino, &node.attr), 0);
+        return reply.entry(
+          &TTL,
+          &self.get_fuser_attr(ino, &node.attr),
+          ino as u64,
+        );
       }
-      None => {
-        reply.error(libc::ENOENT);
-      }
+      None => {}
     }
+    reply.error(libc::ENOENT);
   }
 
   #[tracing::instrument(skip(self, _req, _fh, _lock, reply), level = "debug")]
@@ -233,6 +340,7 @@ impl fuser::Filesystem for FileSystem {
     let file_id = attr.id.clone();
     let new_cached_file =
       self.cached_files.iter().find(|file| file.id == file_id);
+
     if let Some(file) = new_cached_file {
       if file.is_new {
         return reply.data(&file.content.as_slice()[offset..]);
@@ -277,19 +385,24 @@ impl fuser::Filesystem for FileSystem {
     let mut entries = Vec::with_capacity(25);
     entries.push((ino, FileType::Directory, "."));
     entries.push((ino, FileType::Directory, ".."));
-    self.nodes.iter().enumerate().for_each(|(idx, node)| {
-      let attr = &node.attr;
-      if dir_id.as_str()
-        == attr.parent_id.as_ref().map(|s| s.as_str()).unwrap_or("/")
-      {
-        let file_type = if attr.is_directory {
-          FileType::Directory
-        } else {
-          FileType::RegularFile
-        };
-        entries.push((idx, file_type, &attr.name));
-      }
-    });
+    self
+      .nodes
+      .iter()
+      .enumerate()
+      .filter(|n| n.1.archived_at.is_none())
+      .for_each(|(idx, node)| {
+        let attr = &node.attr;
+        if dir_id.as_str()
+          == attr.parent_id.as_ref().map(|s| s.as_str()).unwrap_or("/")
+        {
+          let file_type = if attr.is_directory {
+            FileType::Directory
+          } else {
+            FileType::RegularFile
+          };
+          entries.push((idx, file_type, &attr.name));
+        }
+      });
 
     for (i, entry) in entries.into_iter().enumerate().skip(offset as usize) {
       if reply.add(entry.0 as u64, (i + 1) as i64, entry.1, entry.2) {
@@ -305,38 +418,99 @@ impl fuser::Filesystem for FileSystem {
     _req: &Request<'_>,
     parent: u64,
     name: &OsStr,
-    mode: u32,
-    umask: u32,
-    rdev: u32,
+    _mode: u32,
+    _umask: u32,
+    _rdev: u32,
     reply: ReplyEntry,
   ) {
     match self.nodes.get(parent as usize) {
       Some(parent) => {
-        let attr = DbAttribute {
-          id: nanoid::nanoid!(21),
-          name: name.to_str().expect("Unsupported file name").to_owned(),
-          parent_id: Some(parent.attr.id.to_owned()),
-          created_at: Utc::now().naive_utc(),
-          is_directory: false,
-          ..Default::default()
-        };
-        self.cached_files.push(CachedFile {
-          id: attr.id.to_owned(),
-          content: vec![],
-          is_new: true,
-        });
-        self.nodes.push(Node {
-          attr,
-          ..Default::default()
-        });
-        let ino = self.nodes.len() - 1;
-        reply.entry(&TTL, &self.get_fuser_attr(ino, &self.nodes[ino].attr), 0);
+        let parent_id = parent.attr.id.clone();
+        let ino = self.add_new_node(&parent_id, name, false);
+        reply.entry(
+          &TTL,
+          &self.get_fuser_attr(ino, &self.nodes[ino].attr),
+          ino as u64,
+        );
       }
       _ => {
         reply.error(libc::ENOENT);
         return;
       }
     };
+  }
+
+  fn mkdir(
+    &mut self,
+    _req: &Request<'_>,
+    parent: u64,
+    name: &OsStr,
+    _mode: u32,
+    _umask: u32,
+    reply: ReplyEntry,
+  ) {
+    match self.nodes.get(parent as usize) {
+      Some(parent) => {
+        let parent_id = parent.attr.id.clone();
+        let ino = self.add_new_node(&parent_id, name, true);
+        reply.entry(
+          &TTL,
+          &self.get_fuser_attr(ino, &self.nodes[ino].attr),
+          ino as u64,
+        );
+      }
+      _ => {
+        reply.error(libc::ENOENT);
+        return;
+      }
+    };
+  }
+
+  #[tracing::instrument(skip(self, _req, reply), level = "debug")]
+  fn unlink(
+    &mut self,
+    _req: &Request<'_>,
+    parent: u64,
+    name: &OsStr,
+    reply: ReplyEmpty,
+  ) {
+    match self.nodes.get(parent as usize) {
+      Some(parent) => {
+        let parent_id = parent.attr.id.clone();
+        let dir_ino = self.find_child_ino(&parent_id, name);
+
+        if let Some(ino) = dir_ino {
+          self.nodes[ino].archived_at = Some(Instant::now());
+          return reply.ok();
+        }
+      }
+      _ => {}
+    };
+    reply.error(libc::ENOENT);
+  }
+
+  #[tracing::instrument(skip(self, _req, reply), level = "debug")]
+  fn rmdir(
+    &mut self,
+    _req: &Request<'_>,
+    parent: u64,
+    name: &OsStr,
+    reply: ReplyEmpty,
+  ) {
+    match self.nodes.get(parent as usize) {
+      Some(parent) => {
+        let parent_id = parent.attr.id.clone();
+        let dir_ino = self.find_child_ino(&parent_id, name);
+
+        if let Some(ino) = dir_ino {
+          self.nodes[ino].archived_at = Some(Instant::now());
+          return reply.ok();
+        }
+      }
+      _ => {}
+    };
+
+    reply.error(libc::ENOENT);
   }
 
   #[tracing::instrument(skip(self, reply), level = "debug")]
@@ -396,6 +570,7 @@ impl fuser::Filesystem for FileSystem {
     let file_id = cached_file.id.clone();
     let is_new = cached_file.is_new;
     self.update_file_content_cache(&file_id, offset as usize, data, is_new);
+    self.nodes[ino as usize].attr.size = data.len() as i32;
     reply.written(data.len() as u32);
   }
 }
