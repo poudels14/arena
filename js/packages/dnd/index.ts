0bfa103029908c647eb4775b03411c382ecee256
Sagar <poudels14@gmail.com>
2024-02-27 13:34:45 -0800

    packages/dnd/ moved to portal/dnd

--- js/packages/dnd/index.ts
+++
@@ -1,9 +0,0 @@
-export { DragDropProvider, useDragDropContext } from "./drag-drop-context";
-export type {
-  DragEvent,
-  DragEventHandler,
-  DragEndEvent,
-} from "./drag-drop-context";
-export { createDraggable } from "./draggable";
-export { createDroppable } from "./droppable";
-export { DragOverlay } from "./overlay";
--- js/packages/dnd/utils.ts
+++
@@ -1,9 +0,0 @@
-import { Point } from "./types";
-
-const distanceBetweenPoints = (first: Point, second: Point) => {
-  return Math.sqrt(
-    Math.pow(first[0] - second[0], 2) + Math.pow(first[1] - second[1], 2)
-  );
-};
-
-export { distanceBetweenPoints };
