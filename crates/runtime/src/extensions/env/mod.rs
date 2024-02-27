8f86dc95f611b5a69a1b783de009f4f3f03295ae
Sagar <poudels14@gmail.com>
2024-02-26 18:29:24 -0800

    runtime: dont load .env from extension
      - since .env is already loaded by dagger, no need to load
        .env from extension
      - loading from dagger makes it easier to customize which .env to
        load from using args but loading .env from ext overrides the
        .env loaded by dagger cli

--- crates/runtime/src/extensions/env/mod.rs
+++ crates/runtime/src/extensions/env/mod.rs
@@ -1,5 +1,3 @@
-use std::path::Path;
-
 use anyhow::Result;
 use deno_core::op2;
 use deno_core::Extension;
@@ -10,7 +8,6 @@ use deno_core::OpState;
 use indexmap::IndexMap;
 use serde_json::json;
 
-use crate::config::RuntimeConfig;
 use crate::extensions::BuiltinExtension;
 
 pub fn extension() -> BuiltinExtension {
@@ -33,43 +30,12 @@ pub fn extension() -> BuiltinExtension {
 
 #[op2]
 #[serde]
-fn op_load_env(state: &mut OpState) -> Result<serde_json::Value> {
+fn op_load_env(_state: &mut OpState) -> Result<serde_json::Value> {
   let mut env_vars = IndexMap::new();
-
   std::env::vars().for_each(|(key, value)| {
     if !env_vars.contains_key(&key) {
       env_vars.insert(key, value);
     }
   });
-
-  let mode = std::env::var("MODE").ok().or(env_vars.get("MODE").cloned());
-  if let Some(config) = state.try_borrow::<RuntimeConfig>() {
-    // load env variables from .env files
-    load_env_from_env_file(&mode.unwrap_or_default(), &config.project_root)
-      .unwrap_or(vec![])
-      .iter()
-      .for_each(|(key, value)| {
-        env_vars.insert(key.to_owned(), value.to_owned());
-      });
-  }
-
   Ok(json!(env_vars))
-}
-
-pub fn load_env_from_env_file(
-  mode: &str,
-  root: &Path,
-) -> Option<Vec<(String, String)>> {
-  if mode == "production" {
-    dotenvy::from_filename_iter(root.join(".env")).ok()
-  } else {
-    dotenvy::from_filename_iter(root.join(".env.dev")).ok()
-  }
-  .map(|envs| {
-    envs
-      .into_iter()
-      .filter(|e| e.is_ok())
-      .map(|e| e.unwrap())
-      .collect()
-  })
 }
