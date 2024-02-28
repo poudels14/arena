fbbdff17a3c3872ed215c74857f2a1c622cf4c70
Sagar <poudels14@gmail.com>
2024-02-27 20:49:46 -0800

    workspace-cluster: add autoprefixer

--- js/apps/atlasai/api/chat/llmQueryContext.ts
+++ js/apps/atlasai/api/chat/llmQueryContext.ts
@@ -8,16 +8,16 @@ function createQueryContextExtension(searchResults: Search.Response[]) {
         const files = searchResults.flatMap((result) => result.files || []);
         if (files.length > 0) {
           request.messages.push({
-            role: "user",
+            role: "system",
             content:
-              `Here are some files that might be related to the my query. Use this information to if it is applicable, else, ignore the provided content and answer from your own knowledge.` +
+              `Here are some files that might be related to users query. Use this information to if it is applicable, else, ignore the provided content and answer from your own knowledge.` +
               `\n` +
               files
                 .map((file) => {
                   return (
                     `Filename: ${file.name}\n` +
                     "Content: " +
-                    file.chunks.map((c) => c.content).join(" ")
+                    file.chunks.map((c) => c.content)
                   );
                 })
                 .join("\n") +
