aaf45eea0aca6de20620cfaf05ba4ba2eb7e9ef7
Sagar Poudel <poudels14@gmail.com>
2024-04-03 00:39:55 -0700

    website: add plausible tracking

--- js/website/src/entry-server.tsx
+++ js/website/src/entry-server.tsx
@@ -10,6 +10,11 @@ export default createHandler(() => (
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="preconnect" href="https://rsms.me/" />
           <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+          <script
+            defer
+            data-domain="useportal.ai"
+            src="https://plausible.io/js/script.js"
+          ></script>
           {assets}
         </head>
         <body class="hiddens scroll:w-1 thumb:rounded thumb:bg-slate-700 track:bg-slate-900">
