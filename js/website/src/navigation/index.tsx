5e52f384635d1870f868b4c077ba0e97d584ead5
Sagar Poudel <poudels14@gmail.com>
2024-04-07 17:45:42 -0700

    website: more nav bar stlying

--- js/website/src/navigation/index.tsx
+++ js/website/src/navigation/index.tsx
@@ -3,17 +3,19 @@ import PortalLogo from "../icons/portal.png";
 export default function NavigationBar() {
   return (
     <nav class="sticky top-0 z-[99999] mx-auto text-center bg-white/95 dark:bg-slate-900/90 backdrop-blur-sm shadow-sm dark:shadow-slate-800 whitespace-nowrap">
-      <div class="flex px-4 py-3 items-center space-x-16">
+      <div class="flex px-4 py-3 items-center space-x-10 md:space-x-16">
         <a
-          class="flex px-4 text-xl font-bold dark:text-gray-100 items-center space-x-2"
+          class="flex text-xl font-bold dark:text-gray-100 items-center space-x-2"
           href="/"
         >
           <img src={PortalLogo} width="32px" />
           <div>Portal</div>
         </a>
-        <div class="hidden md:flex flex-1 dark:text-gray-400 text-sm font-medium items-center space-x-5">
-          <NavItem title="Discord" href="https://discord.gg/kX4fYm7c" />
-          <NavItem title="Download" href="/desktop" />
+        <div class="flex-1 dark:text-gray-400 text-sm font-medium">
+          <div class="hidden md:flex justify-start space-x-5">
+            <NavItem title="Discord" href="https://discord.gg/kX4fYm7c" />
+            <NavItem title="Download" href="/desktop" />
+          </div>
         </div>
         <div class="px-4 flex dark:text-white text-sm items-center space-x-4">
           <a
