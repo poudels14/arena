26c8ac4702552cf1f8461830367260b850e8db9a
Sagar <poudels14@gmail.com>
2024-04-17 02:10:49 -0700

    atlasai: improve title generation

--- js/apps/atlasai/api/chat/chains/title.ts
+++ js/apps/atlasai/api/chat/chains/title.ts
@@ -6,9 +6,10 @@ import { Workspace } from "@portal/workspace-sdk";
 async function generateQueryTitle(model: Workspace.Model, query: string) {
   const prompt = ChatPromptTemplate.fromMessages([
     [
-      "human",
-      "You are an expert at generating title of a user query. Don't use quotes. Keep it short and simple. Generate a title for follow query without any explanation: {query}",
+      "system",
+      "You are an AI called Atlas who is expert at generating a summary of any text. You should ALWAYS respond with a SHORT and HELPFUL summary without using QUOTES OR providing additional info.",
     ],
+    ["human", "Summarize the text in few words: {input}"],
   ]);
 
   const chainModel = getLLMModel(model, {
@@ -17,7 +18,7 @@ async function generateQueryTitle(model: Workspace.Model, query: string) {
   const outputParser = new StringOutputParser();
   const chain = prompt.pipe(chainModel).pipe(outputParser);
   const title = await chain.invoke({
-    query,
+    input: query,
   });
 
   // replace quotes if any
