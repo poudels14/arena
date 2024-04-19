75152ecb3a55c0ea7164df1a3ed28929fa17a73a
Sagar Poudel <poudels14@gmail.com>
2024-04-19 00:11:06 -0700

    portal: add lib.rs

---
+++ crates/portal/src/lib.rs
@@ -0,0 +1,15 @@
+use napi::{Error, Result, Status};
+use napi_derive::napi;
+
+mod config;
+mod database;
+mod portal;
+mod server;
+mod utils;
+mod workspace;
+
+#[napi]
+pub fn start_portal_server(port: u16) -> Result<()> {
+  portal::run_portal(portal::Command::Start(server::Command { port }))
+    .map_err(|e| Error::new(Status::GenericFailure, format!("{:?}", e)))
+}
