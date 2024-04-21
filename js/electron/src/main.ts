6761934bdb81fc784d81a7befa2c9216417148f9
Sagar Poudel <poudels14@gmail.com>
2024-04-21 12:25:23 -0700

    electron: add edit sub menu
      - without this, copy/paste doesn't work

--- js/electron/src/main.ts
+++ js/electron/src/main.ts
@@ -32,8 +32,24 @@ const createWindow = () => {
 const setupMenu = () => {
   const menuTemplate = [
     {
-      label: "File",
+      label: app.name,
       submenu: [{ role: "quit" }],
+    },
+    {
+      label: "Edit",
+      submenu: [
+        { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+        { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+        { type: "separator" },
+        { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+        {
+          label: "Select All",
+          accelerator: "CmdOrCtrl+A",
+          selector: "selectAll:",
+        },
+      ],
     },
   ];
 
