c9fdfcafed0f3365b86f4259e376ef707dabf9a3
Sagar Poudel <poudels14@gmail.com>
2024-04-22 15:30:59 -0700

    portal: add function to napi module

--- crates/portal/src/lib.rs
+++ crates/portal/src/lib.rs
@@ -1,3 +1,5 @@
+use arenasql::chrono::{DateTime, Local};
+use common::dirs;
 use napi::{Error, Result, Status};
 use napi_derive::napi;
 
@@ -18,4 +20,23 @@ pub fn start_portal_server(port: u16) -> Result<()> {
 pub fn reset_data() -> Result<()> {
   portal::run_portal(portal::Command::Reset)
     .map_err(|e| Error::new(Status::GenericFailure, format!("{:?}", e)))
+}
+
+#[napi]
+pub fn get_todays_log_file_name() -> Result<String> {
+  let local: DateTime<Local> = Local::now();
+  dirs::portal()
+    .map_err(|e| Error::new(Status::GenericFailure, format!("{:?}", e)))?
+    .cache_dir()
+    .join("logs")
+    .join(&format!("portal.log.{}", local.format("%Y-%m-%d")))
+    .as_os_str()
+    .to_str()
+    .map(|s| s.to_owned())
+    .ok_or_else(|| {
+      Error::new(
+        Status::GenericFailure,
+        format!("Error converting path to string"),
+      )
+    })
 }
