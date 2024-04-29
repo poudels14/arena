f9d552069126c6e9ca3e3a8b8e805a84580943e5
Sagar Poudel <poudels14@gmail.com>
2024-04-29 01:28:16 -0700

    use @porta/cortex for title generation
      - @porta/cortex doesn't support streaming + context resolution yet

--- js/apps/atlasai/api/chat/chains/modelSelector.ts
+++ js/apps/atlasai/api/chat/chains/modelSelector.ts
@@ -1,8 +1,15 @@
+import path from "path";
 import { Workspace } from "@portal/workspace-sdk";
 import { ChatOpenAI } from "@langchain/openai";
 import { ChatGroq } from "@langchain/groq";
 import { ChatOllama } from "@langchain/community/chat_models/ollama";
 import { ChatAnthropic } from "@langchain/anthropic";
+import {
+  AnthropicChat,
+  GenericChatModel,
+  Groq,
+  OllamaChat,
+} from "@portal/cortex/integrations/models";
 
 import { ChatPortalAI } from "./chat_model";
 
@@ -62,4 +69,70 @@ const getLLMModel = (
   }
 };
 
-export { getLLMModel };
+const buildModelProvider = (model: Workspace.Model) => {
+  switch (model.provider) {
+    case "openai": {
+      return new GenericChatModel({
+        url: "https://api.openai.com/v1/chat/completions",
+        model: model.config.model.name,
+        apiKey: model.config.http.apiKey,
+        // TODO
+        contextLength: 8000,
+      });
+    }
+    case "anthropic": {
+      return new AnthropicChat({
+        model: model.config.model.name,
+        apiKey: model.config.http.apiKey!,
+        version: "2023-06-01",
+        // TODO
+        contextLength: 8000,
+      });
+    }
+    case "groq": {
+      return new Groq({
+        apiKey: model.config.http.apiKey,
+        model: model.config.model.name || "mixtral-8x7b-32768",
+      });
+    }
+    case "portal": {
+      return new GenericChatModel({
+        // TODO: remove /chat/completions suffix after atlasi ai is migrated to using
+        // @portal/cortex and require that suffix in settings
+        url: path.join(model.config.http.endpoint!, "chat/completions"),
+        model: model.config.model.name,
+        contextLength: 8000,
+      });
+    }
+    case "ollama": {
+      return new OllamaChat({
+        // TODO: remove /chat/completions suffix after atlasi ai is migrated to using
+        // @portal/cortex and require that suffix in settings
+        url: path.join(
+          model.config.http.endpoint || "http://localhost:11434/",
+          "/api/chat"
+        ),
+        model: model.config.model.name,
+        // TODO
+        contextLength: 8000,
+      });
+    }
+    case "lmstudio": {
+      return new GenericChatModel({
+        // TODO: remove /chat/completions suffix after atlasi ai is migrated to using
+        // @portal/cortex and require that suffix in settings
+        url: path.join(
+          model.config.http.endpoint || "http://localhost:1234/v1/",
+          "chat/completions"
+        ),
+        model: "unknown",
+        // TODO
+        contextLength: 8000,
+      });
+    }
+    default:
+      throw new Error("Unsupported model provider: " + model.provider);
+  }
+};
+
+export { getLLMModel, buildModelProvider };
--- js/apps/atlasai/api/chat/chains/title.ts
+++ js/apps/atlasai/api/chat/chains/title.ts
@@ -1,28 +1,35 @@
-import { ChatPromptTemplate } from "@langchain/core/prompts";
-import { StringOutputParser } from "@langchain/core/output_parsers";
-import { getLLMModel } from "./modelSelector";
 import { Workspace } from "@portal/workspace-sdk";
+import { ChatCompletionExecutor } from "@portal/cortex/executors/ChatCompletion";
+import { ChatPromptTemplate } from "@portal/cortex/prompt";
+import { parseStringResponse } from "@portal/cortex/plugins/response";
 
-async function generateQueryTitle(model: Workspace.Model, query: string) {
+import { buildModelProvider } from "./modelSelector";
+
+async function generateQueryTitle(modelInfo: Workspace.Model, query: string) {
   const prompt = ChatPromptTemplate.fromMessages([
     [
       "system",
       "You are an AI called Atlas who is expert at generating a summary of any text. You should ALWAYS respond with a SHORT and HELPFUL summary without using QUOTES OR providing additional info.",
     ],
-    ["human", "Summarize the text in few words: {input}"],
+    ["human", "Summarize the text in few words: {query}"],
   ]);
 
-  const chainModel = getLLMModel(model, {
-    temperature: 0.9,
-  });
-  const outputParser = new StringOutputParser();
-  const chain = prompt.pipe(chainModel).pipe(outputParser);
-  const title = await chain.invoke({
-    input: query,
+  const model = buildModelProvider(modelInfo);
+  const executor = new ChatCompletionExecutor({
+    runnables: [prompt, model],
   });
 
-  // replace quotes if any
-  return title.replaceAll(/(^")|("$)/g, "");
+  const title = await executor
+    .invoke({
+      variables: {
+        query,
+      },
+      config: {
+        temperature: 0.5,
+      },
+    })
+    .then(parseStringResponse);
+  return title;
 }
 
 export { generateQueryTitle };
