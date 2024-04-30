56f69bafe8787b3486a8ce21e419470a255a979d
Sagar Poudel <poudels14@gmail.com>
2024-04-30 01:22:31 -0700

    atlasai: completely remove langchain dep

--- js/apps/atlasai/api/chat/chains/modelSelector.ts
+++ js/apps/atlasai/api/chat/chains/modelSelector.ts
@@ -1,73 +1,11 @@
 import path from "path";
 import { Workspace } from "@portal/workspace-sdk";
-import { ChatOpenAI } from "@langchain/openai";
-import { ChatGroq } from "@langchain/groq";
-import { ChatOllama } from "@langchain/community/chat_models/ollama";
-import { ChatAnthropic } from "@langchain/anthropic";
 import {
   AnthropicChat,
   GenericChatModel,
   Groq,
   OllamaChat,
 } from "@portal/cortex/integrations/models";
-
-import { ChatPortalAI } from "./chat_model";
-
-const getLLMModel = (
-  model: Workspace.Model,
-  options: { temperature: number }
-) => {
-  switch (model.provider) {
-    case "groq": {
-      return new ChatGroq({
-        apiKey: model.config.http.apiKey,
-        modelName: model.config.model.name || "mixtral-8x7b-32768",
-        temperature: options.temperature,
-      });
-    }
-    case "ollama": {
-      return new ChatOllama({
-        baseUrl:
-          model.config.http.endpoint ||
-          "http://localhost:1234/v1/chat/completions",
-        model: model.config.model.name || "mistral",
-        temperature: options.temperature,
-      });
-    }
-    case "lmstudio": {
-      return new ChatPortalAI({
-        portalAIbaseUrl:
-          model.config.http.endpoint || "http://localhost:1234/v1/",
-        portalAIApiKey: "n/a",
-        temperature: options.temperature,
-      });
-    }
-    case "portal": {
-      return new ChatPortalAI({
-        portalAIbaseUrl: model.config.http.endpoint!,
-        portalAIApiKey: "n/a",
-        temperature: options.temperature,
-        modelName: model.config.model.name,
-      });
-    }
-    case "openai": {
-      return new ChatOpenAI({
-        openAIApiKey: model.config.http.apiKey,
-        modelName: model.config.model.name,
-        temperature: options.temperature,
-      });
-    }
-    case "anthropic": {
-      return new ChatAnthropic({
-        anthropicApiKey: model.config.http.apiKey,
-        modelName: model.config.model.name,
-        temperature: options.temperature,
-      });
-    }
-    default:
-      throw new Error("Unsupported model provider: " + model.provider);
-  }
-};
 
 const buildModelProvider = (model: Workspace.Model) => {
   switch (model.provider) {
@@ -135,4 +73,4 @@ const buildModelProvider = (model: Workspace.Model) => {
   }
 };
 
-export { getLLMModel, buildModelProvider };
+export { buildModelProvider };
