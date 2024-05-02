a453e1a1648b81f4d38575155719c9ba858eb57d
Sagar Poudel <poudels14@gmail.com>
2024-05-02 15:10:58 -0700

    website: add route to check for desktop updates
      - it makes more sense for website to have info on updates
        than workspace-cluster

--- js/website/src/routes/[...404].tsx
+++ js/website/src/routes/[...404].tsx
@@ -1,10 +1,10 @@
 export default function NotFound() {
   return (
-    <main class="text-center mx-auto text-gray-700 p-4">
+    <main class="text-center mx-auto text-gray-700 p-4 py-36">
       <h1 class="mt-24 max-6-xs text-6xl font-bold text-gray-700">Not Found</h1>
       <p class="mt-8">
         Go to{" "}
-        <a href="https://sidecar.so" class="text-blue-800 hover:underline">
+        <a href="/" class="text-blue-800 hover:underline">
           Homepage
         </a>
       </p>
---
+++ js/website/src/routes/desktop/updates/[tag].ts
@@ -0,0 +1,40 @@
+import semver from "semver";
+import { APIEvent } from "@solidjs/start/server";
+
+const MAC_VERSIONS = ["0.1.9", "0.1.10", "0.1.11", "0.1.12"];
+export async function GET({ params }: APIEvent) {
+  const regex = [...params.tag.matchAll(/(\w+)-(\w+)-([0-9\.]+)/gm)];
+  const os = regex[0][1];
+  const arch = regex[0][2];
+  const currentVersion = regex[0][3];
+  try {
+    if (os == "darwin") {
+      const newVersion = MAC_VERSIONS.findLast((version) =>
+        semver.gt(version, currentVersion)
+      );
+      console.log(
+        `new version for mac [current=${currentVersion}, arch=${arch}]: `,
+        newVersion
+      );
+      if (newVersion) {
+        return {
+          version: newVersion,
+          pub_date: new Date().toISOString(),
+          url: `https://github.com/poudels14/portal-release/releases/download/${newVersion}/Portal_${newVersion}_${arch}_darwin.zip`,
+        };
+      }
+    }
+  } catch {}
+
+  return new Response(null, {
+    status: 204,
+  });
+
+  // return {
+  //   version: newVersion,
+  //   pub_date: new Date().toISOString(),
+  //   url: `https://github.com/poudels14/portal-release/releases/download/${newVersion}/Portal_${newVersion}_${arch}.dmg`,
+  //   signature: "Content of the relevant .sig file",
+  //   notes: "These are some release notes",
+  // };
+}
