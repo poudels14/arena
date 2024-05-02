a421030114c06f5449c5bdff993e92987b6aef13
Sagar Poudel <poudels14@gmail.com>
2024-05-02 16:49:00 -0700

    website: release 0.1.13 mac version

--- js/website/portal.app.cjs
+++ js/website/portal.app.cjs
@@ -3,7 +3,7 @@ const pkg = require("./package.json");
 /** @type {import('@portal/sdk/app/build').AppConfig} */
 module.exports = {
   id: "portal-website",
-  version: "2024-04-23-20-15",
+  version: "2024-05-02-16-46",
   registry: {
     host: "http://localhost:9001/",
     apiKey: process.env.REGISTRY_API_KEY,
